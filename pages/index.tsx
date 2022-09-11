4cc73135935e6e5c679af8dfdf16beccd8be148a
shotasz <s2shota123@gmail.com>
2022-09-11 09:54:56 +0900

    フックuseReducer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,80 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { createContext, ReactElement, ReactNode } from 'react'
+import {
+  createContext,
+  ReactElement,
+  ReactNode,
+  useReducer,
+  useState
+} from 'react'
 import styles from '../styles/Home.module.css'
 
-interface Engineer {
-  name: string
-  social?: {
-    facebook: boolean
-    twitter: boolean
+// interface Engineer {
+//   name: string
+//   social?: {
+//     facebook: boolean
+//     twitter: boolean
+//   }
+// }
+
+// type ContainerProps = {
+//   title: string
+//   children: ReactNode
+// }
+
+// const TitleContext = createContext('')
+
+// const Title = () => {
+//   return (
+//     <TitleContext.Consumer>
+//       {(title) => {
+//         return <h1>{title}</h1>
+//       }}
+//     </TitleContext.Consumer>
+//   )
+// }
+
+// const Header = () => {
+//   return (
+//     <div>
+//       <Title />
+//     </div>
+//   )
+// }
+
+type Action = 'DECREMENT' | 'INCREMENT' | 'DOUBLE' | 'RESET'
+
+const Home = () => {
+  const initialValue = { count: 0 }
+  const reducer = (currentCount: number, action: Action) => {
+    switch (action) {
+      case 'INCREMENT':
+        return currentCount + 1
+
+      case 'DECREMENT':
+        return currentCount - 1
+
+      case 'DOUBLE':
+        return currentCount * 2
+
+      case 'RESET':
+        return 0
+
+      default:
+        return currentCount
+    }
   }
-}
 
-type ContainerProps = {
-  title: string
-  children: ReactNode
-}
-
-const TitleContext = createContext('')
-
-const Title = () => {
-  return (
-    <TitleContext.Consumer>
-      {(title) => {
-        return <h1>{title}</h1>
-      }}
-    </TitleContext.Consumer>
-  )
-}
-
-const Header = () => {
-  return (
-    <div>
-      <Title />
-    </div>
-  )
-}
-
-const Home: NextPage = () => {
-  const title = 'React Book'
+  const [count, dispatch] = useReducer(reducer, initialValue.count)
 
   return (
     <div className={styles.container}>
-      <TitleContext.Provider value="title">
-        <Header />
-      </TitleContext.Provider>
+      <p>{`Count : ${count}`}</p>
+      <button onClick={() => dispatch('DECREMENT')}>-</button>
+      <button onClick={() => dispatch('INCREMENT')}>+</button>
+      <button onClick={() => dispatch('DOUBLE')}>x2</button>
+      <button onClick={() => dispatch('RESET')}>Reset</button>
     </div>
   )
 }
