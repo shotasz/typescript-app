af5ca211eb4e5a079a81cbd5893ee44d64a0bf1f
shotasz <s2shota123@gmail.com>
2022-09-11 10:41:30 +0900

    フックuseCallback

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,78 +3,57 @@ import Head from 'next/head'
 import Image from 'next/image'
 import {
   createContext,
+  memo,
   ReactElement,
   ReactNode,
+  useCallback,
   useReducer,
   useState
 } from 'react'
 import styles from '../styles/Home.module.css'
 
-// interface Engineer {
-//   name: string
-//   social?: {
-//     facebook: boolean
-//     twitter: boolean
-//   }
-// }
-
-// type ContainerProps = {
-//   title: string
-//   children: ReactNode
-// }
-
-// const TitleContext = createContext('')
-
-// const Title = () => {
-//   return (
-//     <TitleContext.Consumer>
-//       {(title) => {
-//         return <h1>{title}</h1>
-//       }}
-//     </TitleContext.Consumer>
-//   )
-// }
-
-// const Header = () => {
-//   return (
-//     <div>
-//       <Title />
-//     </div>
-//   )
-// }
-
+type ButtonProps = { onClick: () => void }
 type Action = 'DECREMENT' | 'INCREMENT' | 'DOUBLE' | 'RESET'
 
+const DecrementButton = (props: ButtonProps) => {
+  const { onClick } = props
+  console.log('Decrementが再描画しました')
+
+  return <button onClick={onClick}>Decrement</button>
+}
+const IncrementButton = memo((props: ButtonProps) => {
+  const { onClick } = props
+  console.log('Incrementが再描画しました')
+
+  return <button onClick={onClick}>Increment</button>
+})
+
+const DoubleButton = memo((props: ButtonProps) => {
+  const { onClick } = props
+  console.log('Doubleが再描画しました')
+
+  return <button onClick={onClick}>Double</button>
+})
+
 const Home = () => {
-  const initialValue = { count: 0 }
-  const reducer = (currentCount: number, action: Action) => {
-    switch (action) {
-      case 'INCREMENT':
-        return currentCount + 1
-
-      case 'DECREMENT':
-        return currentCount - 1
-
-      case 'DOUBLE':
-        return currentCount * 2
-
-      case 'RESET':
-        return 0
-
-      default:
-        return currentCount
-    }
+  const [count, setCount] = useState(0)
+  const decrement = () => {
+    setCount((c) => c - 1)
+  }
+  const increment = () => {
+    setCount((c) => c + 1)
   }
 
-  const [count, dispatch] = useReducer(reducer, initialValue.count)
+  const double = useCallback(() => {
+    setCount((c) => c * 2)
+  }, [])
 
   return (
     <div className={styles.container}>
       <p>{`Count : ${count}`}</p>
-      <button onClick={() => dispatch('DECREMENT')}>-</button>
-      <button onClick={() => dispatch('INCREMENT')}>+</button>
-      <button onClick={() => dispatch('DOUBLE')}>x2</button>
-      <button onClick={() => dispatch('RESET')}>Reset</button>
+      <DecrementButton onClick={decrement} />
+      <IncrementButton onClick={increment} />
+      <DoubleButton onClick={double} />
     </div>
   )
 }
