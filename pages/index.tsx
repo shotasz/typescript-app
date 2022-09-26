e511b4bd7fdeafb636c49055f3b6a7df45f89f0f
shotasz <s2shota123@gmail.com>
2022-09-26 21:25:32 +0900

    PresentationalComponent,ContainerComponent

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,59 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { ChangeEvent, useMemo, useState, useEffect } from 'react'
+import { ChangeEvent, useMemo, useState, useEffect, useCallback } from 'react'
 import Link from 'next/link'
 
 import styles from '../styles/Home.module.css'
 import { useRouter } from 'next/router'
+import Button from '../components/Button'
 
-const Home: NextPage = () => {
-  const router = useRouter()
+const usePopup = () => {
+  const cb = useCallback((text: string) => {
+    prompt(text)
+  }, [])
 
-  const onSubmit = () => {
-    router.reload()
-  }
+  return cb
+}
 
-  const onSubmit2 = () => {
-    router.back()
-  }
+type CountButtonProps = {
+  label: string
+  maxium: number
+}
+
+const Home = (props: CountButtonProps) => {
+  const { label, maxium } = props
+
+  const displayPopup = usePopup()
+
+  const [count, setCount] = useState(0)
+
+  const onClick = useCallback(() => {
+    const newCount = count + 1
+    setCount(newCount)
+
+    if (newCount >= maxium) {
+      displayPopup(`You clicked ${newCount} times!`)
+    }
+  }, [count, maxium])
+
+  const disabled = count >= maxium
+
+  const text = disabled ? 'Cant click any more' : `You clicked ${count} times!`
+  // const router = useRouter()
+
+  // const onSubmit = () => {
+  //   router.reload()
+  // }
+
+  // const onSubmit2 = () => {
+  //   router.back()
+  // }
 
   return (
     <div className={styles.container}>
       <p>Hello World</p>
-      <>
+      {/* <>
         <Link
           href={{
             pathname: '/ssr',
@@ -37,26 +69,10 @@ const Home: NextPage = () => {
         <Link href="/ssg">
           <button>Jump to SSG Page</button>
         </Link>
-      </>
-      <button onClick={onSubmit}>Reload Page</button>
-      <button onClick={onSubmit2}>Back Page</button>
-
-      <div>
-        <div>
-          <img
-            className={styles.images}
-            src="/pexels-alberta-studios-10445627.jpg"
-            alt="Image IMG"
-          />
-        </div>
-        <div>
-          <Image
-            src="/pexels-alberta-studios-10445627.jpg"
-            width={100}
-            height={100}
-          />
-        </div>
-      </div>
+      </> */}
+      {/* <button onClick={onSubmit}>Reload Page</button>
+      <button onClick={onSubmit2}>Back Page</button> */}
+      <Button disabled={disabled} onClick={onClick} label={label} text={text} />
     </div>
   )
 }
