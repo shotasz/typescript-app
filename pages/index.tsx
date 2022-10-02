b5477e6a10e87608d71933771665b7e455b53955
shotasz <s2shota123@gmail.com>
2022-10-02 09:13:23 +0900

    storybook導入、StyledButtonコミット

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,78 +1,42 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { ChangeEvent, useMemo, useState, useEffect, useCallback } from 'react'
-import Link from 'next/link'
+import { PropsWithChildren, ReactNode } from 'react'
+import Link, { LinkProps } from 'next/link'
 
 import styles from '../styles/Home.module.css'
+import styled, { css } from 'styled-components'
 import { useRouter } from 'next/router'
-import Button from '../components/Button'
 
-const usePopup = () => {
-  const cb = useCallback((text: string) => {
-    prompt(text)
-  }, [])
-
-  return cb
+type BaseLinkProps = PropsWithChildren<LinkProps> & {
+  className?: string
+  children: ReactNode
 }
 
-type CountButtonProps = {
-  label: string
-  maxium: number
-}
-
-const Home = (props: CountButtonProps) => {
-  const { label, maxium } = props
-
-  const displayPopup = usePopup()
-
-  const [count, setCount] = useState(0)
-
-  const onClick = useCallback(() => {
-    const newCount = count + 1
-    setCount(newCount)
-
-    if (newCount >= maxium) {
-      displayPopup(`You clicked ${newCount} times!`)
-    }
-  }, [count, maxium])
-
-  const disabled = count >= maxium
-
-  const text = disabled ? 'Cant click any more' : `You clicked ${count} times!`
-  // const router = useRouter()
-
-  // const onSubmit = () => {
-  //   router.reload()
-  // }
-
-  // const onSubmit2 = () => {
-  //   router.back()
-  // }
+const BaseLink = (props: BaseLinkProps) => {
+  const { className, children, ...rest } = props
 
   return (
+    <Link {...rest}>
+      <a className={className}>{children}</a>
+    </Link>
+  )
+}
+
+const StyledLink = styled(BaseLink)`
+  color: #1e90ff;
+  font-size: 2em;
+`
+
+const Text = styled.span`
+  color: ${(props) => props.theme.colors.red};
+`
+
+const Home: NextPage = () => {
+  return (
     <div className={styles.container}>
-      <p>Hello World</p>
-      {/* <>
-        <Link
-          href={{
-            pathname: '/ssr',
-            query: {
-              keyword: 'hello'
-            }
-          }}
-        >
-          <a>Go to SSR</a>
-        </Link>
-      </>
-      <>
-        <Link href="/ssg">
-          <button>Jump to SSG Page</button>
-        </Link>
-      </> */}
-      {/* <button onClick={onSubmit}>Reload Page</button>
-      <button onClick={onSubmit2}>Back Page</button> */}
-      <Button disabled={disabled} onClick={onClick} label={label} text={text} />
+      <StyledLink href="/">Go to Index</StyledLink>
+      <Text>Themeから参照した</Text>
     </div>
   )
 }
---
+++ theme.ts
@@ -0,0 +1,8 @@
+export const theme = {
+  space: ['0px', '4px', '8px'],
+  colors: {
+    white: '#ffffff',
+    black: '#000000',
+    red: '#ff0000'
+  }
+}
