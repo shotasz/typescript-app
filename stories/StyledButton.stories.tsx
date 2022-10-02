102f0564e996cdedf9ab0956dd68849426d59d51
shotasz <s2shota123@gmail.com>
2022-10-02 09:28:08 +0900

    storybook,Actionを使用したコールバック

--- stories/StyledButton.stories.tsx
+++ stories/StyledButton.stories.tsx
@@ -1,15 +1,26 @@
+import { MouseEvent, useState } from 'react'
 import { ComponentMeta } from '@storybook/react'
 import { StyledButton } from '../components/StyledButton'
+
+import { action } from '@storybook/addon-actions'
 
 export default {
   title: 'StyledButton',
   component: StyledButton
 } as ComponentMeta<typeof StyledButton>
 
+const incrementAction = action('increment')
+
 export const Primary = (props: any) => {
+  const [count, setCount] = useState(0)
+  const onClick = (e: MouseEvent) => {
+    incrementAction(e, count)
+    setCount((c) => c + 1)
+  }
+
   return (
-    <StyledButton {...props} variant="primary">
-      Primary
+    <StyledButton {...props} variant="primary" onClick={onClick}>
+      Count: {count}
     </StyledButton>
   )
 }
